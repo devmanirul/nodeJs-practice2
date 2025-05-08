c0862046d83f211bbdbc0b876522c6e135e81e34
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2025-05-08 23:28:05 +0600

    first commit

---
+++ calculation.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const multi = (a, b) => {
+  return a * b;
+};
+const calculation = { sum, sub, multi };
+export default calculation;
---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log("Hello Bangladesh");
+// console.log({
+//   name: "Mahbubul Alam",
+//   age: 34,
+// });
+
+// console.table({
+//   name: "Mahabub Alam",
+//   age: 34,
+// });
+
+// let a = 20;
+// let b = 50;
+// let sum = a + b;
+// console.log(sum);
+
+// modules = a set of function
+// import calculation from "./calculation.js";
+
+// const output = calculation.sum(10, 20);
+// const output2 = calculation.sub(50, 5);
+// const output3 = calculation.multi(50, 5);
+// console.log(output, output2, output3);
+
+// build in modules
